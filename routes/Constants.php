0a44c427a23dbe6bdf05c5acf977caf14461894a
TOLANY-LANNIE <tolanym_2@live.com>
2021-09-02 23:17:05 +0200

    test

--- routes/Constants.php
+++ routes/Constants.php
@@ -3,7 +3,7 @@
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASSWORD', '');
-    define('DB_NAME', 'costalldb');
+    define('DB_NAME', 'costalldb'); //
 
 
     define('RECORD_CREATED_SUCCESSFULLY', 101);
