a9abe1d632f4c773bec2b2db5c4dd95fe97899a9
TOLANY-LANNIE <tolanym_2@live.com>
2021-09-03 01:57:15 +0200

    added method signatures

--- routes/UserOperations.php
+++ routes/UserOperations.php
@@ -45,22 +45,37 @@
         public function updateUserName($userID,$currentUsername, $newUsername){
             
         }
-
-        /*
-         METHOD: gets all user in the system 
+         /*
+         METHOD: 
          PARAMS: non
         */
-        public function getAllUsers(){
-         
-            $stmt = $this->con->prepare("SELECT Registration_ID,Name,Surname,Profession,o.Organisation_Name,o.Department FROM registration_info r INNER JOIN organisation o ON r.Organisation_ID = o.Organisation_ID");
-            $stmt->execute();
-            $result = $stmt->get_result(); // get the mysqli result
-            $rows = array();
-            while ($row =$result->fetch_assoc()) {
-               array_push($rows, $row);
-            }
-            return $rows;
+        private function deleteUserAccount($userID,$currentUsername, $newUsername){
+            
         }
+
+
+        /*
+        METHOD: 
+        PARAMS:
+        */
+        public function getUserInfo($username, $password) {
+            $userID = $this->userLogin($username, $password);
+            $stmt = $this->con->prepare("SELECT r.Name, r.Surname, r.Email, r.User_ID, r.registration_id FROM registration_info r INNER JOIN user_account u ON r.user_id = u.user_id WHERE u.User_ID = ? ;");
+            $stmt->bind_param("s", $userID);
+            if ($stmt->execute()) {
+               $result = $stmt->get_result();
+               $userInfo = array();
+               while ($row = $result->fetch_assoc()) {
+                array_push($userInfo, $row);
+               }
+               return $userInfo;
+               return USER_DETAILS_RETRIEVED;
+            }else {
+   
+            }
+            
+         }
+   
 
 
         /*
@@ -75,6 +90,22 @@
             $stmt->fetch(); 
             return $id; 
 
+        }
+
+          /*
+         METHOD: gets all user in the system 
+         PARAMS: non
+        */
+        public function getAllUsers(){
+         
+            $stmt = $this->con->prepare("SELECT Registration_ID,Name,Surname,Profession,o.Organisation_Name,o.Department FROM registration_info r INNER JOIN organisation o ON r.Organisation_ID = o.Organisation_ID");
+            $stmt->execute();
+            $result = $stmt->get_result(); // get the mysqli result
+            $rows = array();
+            while ($row =$result->fetch_assoc()) {
+               array_push($rows, $row);
+            }
+            return $rows;
         }
 }
 
